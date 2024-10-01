22de2288bf7b79e54d221c98555fd7641adef152
syncxyzz <contanitro01v@gmail.com>
2024-10-01 16:20:15 +0100

    Add files via upload

---
+++ on.bat
@@ -0,0 +1,9 @@
+::                                                            credits:⚡Mr.Draco⚡#0467
+
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_nxt\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi64_hyperv\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi2\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files\BlueStacks_msi5\HD-Player.exe"
+netsh advfirewall firewall delete rule name=all program="C:\Program Files (x86)\SmartGaGa\ProjectTitan\Engine\ProjectTitan.exe"
+::                                                            credits:⚡Mr.Draco⚡#0467
